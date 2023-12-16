47b5920f311726887969e257d32edfbc95132f6c
Maksym <max.sheremeta228@gmail.com>
2023-12-16 14:20:32 +0200

    Add Create, edit, delete test

--- ShoppingSystemWebTest/ProductsControllerTest.cs
+++ ShoppingSystemWebTest/ProductsControllerTest.cs
@@ -14,14 +14,14 @@ namespace ShoppingSystemWeb.Tests;
 
 public class ProductsControllerTest
 {
-    private static readonly Product _testProduct = new Product()
-    {
-        Id = 123,
-        Title = "test title",
-        Category = "test category",
-        ExpiredDate = DateTime.Now,
-        Price = 13.13m                                                                                  
-    };
+    //private static readonly Product _testProduct = new Product()
+    //{
+    //    Id = 123,
+    //    Title = "test title",
+    //    Category = "test category",
+    //    ExpiredDate = DateTime.Now,
+    //    Price = 13.13m
+    //};
 
     //[Fact]
     //public async Task Index_ReturnsAViewResult_WithAListOfProducts()
@@ -39,6 +39,7 @@ public class ProductsControllerTest
     //    var model = Assert.IsAssignableFrom<PaginatedList<Product>>(viewResult.ViewData.Model);
     //    Assert.Equal(3, model.Count);
     //}
+
 
     [Fact]
     public async Task Details_ReturnsNotFoundResult_WhenIdIsNull()
@@ -79,7 +80,7 @@ public class ProductsControllerTest
         Assert.Equal("Index", redirectToActionResult.ActionName);
     }
     [Fact]
-    public async Task Create_AddsProductToMock_WhenModelStateIsValid()
+    public async Task Create_AddsProduct()
     {
         // Arrange
         var mockContext = new Mock<IShoppingSystemWebContext>();
@@ -103,6 +104,97 @@ public class ProductsControllerTest
         mockDbSet.Verify(dbSet => dbSet.Add(It.Is<Product>(p => p == testProduct)), Times.Once);
         mockContext.Verify(context => context.SaveChangesAsync(CancellationToken.None), Times.Once);
     }
+
+    [Fact]
+    public async Task Edit_UpdatesProduct()
+    {
+        // Arrange
+        var mockContext = new Mock<IShoppingSystemWebContext>();
+        var mockDbSet = new Mock<DbSet<Product>>();
+        mockContext.Setup(c => c.Product).Returns(mockDbSet.Object);
+
+        var controller = new ProductsController(mockContext.Object);
+        var testProduct = new Product
+        {
+            Id = 123,
+            Title = "Original Title",
+            Category = "Original Category",
+            ExpiredDate = DateTime.Now,
+            Price = 10.0m
+        };
+
+        // Act
+        await controller.Edit(testProduct.Id, testProduct);
+
+        // Assert
+        mockDbSet.Verify(dbSet => dbSet.Update(It.Is<Product>(p => p == testProduct)), Times.Once);
+        mockContext.Verify(context => context.SaveChangesAsync(CancellationToken.None), Times.Once);
+    }
+
+    [Fact]
+    public async Task DeleteConfirmed_DeletesProduct()
+    {
+        // Arrange
+        var mockContext = new Mock<IShoppingSystemWebContext>();
+        var mockDbSet = new Mock<DbSet<Product>>();
+        mockContext.Setup(c => c.Product).Returns(mockDbSet.Object);
+
+        var controller = new ProductsController(mockContext.Object);
+        var testProductId = 123;
+        var testProduct = new Product
+        {
+            Id = testProductId,
+            Title = "Test Title",
+            Category = "Test Category",
+            ExpiredDate = DateTime.Now,
+            Price = 10.0m
+        };
+
+        // Add the test product to the mock context
+        mockDbSet.Setup(dbSet => dbSet.FindAsync(It.IsAny<object[]>()))
+            .ReturnsAsync((object[] keyValues) => testProduct);
+
+        // Act
+        await controller.DeleteConfirmed(testProductId);
+
+        // Assert
+        mockContext.Verify(context => context.SaveChangesAsync(CancellationToken.None), Times.Once);
+        mockDbSet.Verify(dbSet => dbSet.FindAsync(testProductId), Times.Once);
+        mockDbSet.Verify(dbSet => dbSet.Remove(testProduct), Times.Once);
+    }
+
+    [Fact]
+    public async Task Details_ReturnsCorrectProduct()
+    {
+        // Arrange
+        var mockContext = new Mock<IShoppingSystemWebContext>();
+        var mockDbSet = new Mock<DbSet<Product>>();
+        mockContext.Setup(c => c.Product).Returns(mockDbSet.Object);
+
+        var controller = new ProductsController(mockContext.Object);
+        var testProductId = 123;
+        var testProduct = new Product
+        {
+            Id = testProductId,
+            Title = "Test Title",
+            Category = "Test Category",
+            ExpiredDate = DateTime.Now,
+            Price = 10.0m
+        };
+
+        // Add the test product to the mock context
+        mockDbSet.Setup(dbSet => dbSet.FindAsync(It.IsAny<object[]>()))
+            .ReturnsAsync((object[] keyValues) => testProduct);
+
+        // Act
+        var result = await controller.Details(testProductId);
+
+        // Assert
+        var viewResult = Assert.IsType<ViewResult>(result);
+        var model = Assert.IsAssignableFrom<Product>(viewResult.ViewData.Model);
+        Assert.Equal(testProduct, model);
+    }
+
 
 
     [Fact]
