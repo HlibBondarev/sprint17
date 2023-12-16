f3e80bf65ce4e85460bc28bee1aec3074a7293c6
Maksym <max.sheremeta228@gmail.com>
2023-12-16 13:18:09 +0200

    Create some tets

--- ShoppingSystemWebTest/ProductsControllerTest.cs
+++ ShoppingSystemWebTest/ProductsControllerTest.cs
@@ -1,116 +1,121 @@
+using Xunit;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-using Moq;
 using ShoppingSystemWeb.Controllers;
 using ShoppingSystemWeb.Data;
 using ShoppingSystemWeb.Models;
-using System.Net.Sockets;
-using System.Reflection.Metadata;
+using Moq;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ShoppingSystemWeb.Services;
+using Microsoft.EntityFrameworkCore;
 
-
-namespace ShoppingSystemWebTest;
+namespace ShoppingSystemWeb.Tests;
 
 public class ProductsControllerTest
 {
-    // TODO: 2.a) Create the private Mocker class. It must encapsulate  construction and configuration 
-    //            of all mocks and the system under test:
-    //            - Mock<ShoppingSystemWebContext> _mockContext;
-    //            - Mock<DbSet<Product>> _mockSet;
-
-    private class Mocker
-    {
-
-
-        private readonly ProductsController _productsController;
-        private readonly Mock<ShoppingSystemWebContext> _mockContext =
-            new Mock<ShoppingSystemWebContext>(new Mock<DbContextOptionsBuilder<ShoppingSystemWebContext>>().Object);
-        Mock<DbSet<Product>> _mockSet = new Mock<DbSet<Product>>();
-        public Mock<ProductsController> MockProductsController { get; set; }
-        public Mock<ShoppingSystemWebContext> MockProductsContext { get; set; }
-
-        public Mocker()
-        {
-            var data = new List<Product> { _testProduct }.AsQueryable();
-
-            _mockSet.As<IQueryable<Product>>().Setup(m => m.Provider).Returns(data.Provider);
-            _mockSet.As<IQueryable<Product>>().Setup(m => m.Expression).Returns(data.Expression);
-            _mockSet.As<IQueryable<Product>>().Setup(m => m.ElementType).Returns(data.ElementType);
-            _mockSet.As<IQueryable<Product>>().Setup(m => m.GetEnumerator()).Returns(data.GetEnumerator());
-
-            _mockContext.Setup(m => m.Product).Returns(_mockSet.Object);
-            MockProductsContext = _mockContext;
-            MockProductsController = new Mock<ProductsController>();
-            _productsController = new ProductsController(_mockContext.Object);
-        }
-
-        public ProductsController Build()
-        {
-            return _productsController;
-        }
-    }
-
-    private static readonly int? ID = 123;
     private static readonly Product _testProduct = new Product()
     {
-        Id = ID ?? 0,
+        Id = 123,
         Title = "test title",
         Category = "test category",
         ExpiredDate = DateTime.Now,
-        Price = 13.13m
+        Price = 13.13m                                                                                  
     };
 
+    //[Fact]
+    //public async Task Index_ReturnsAViewResult_WithAListOfProducts()
+    //{
+    //    // Arrange
+    //    var mockContext = new Mock<IShoppingSystemWebContext>();
+    //    mockContext.Setup(ctx => ctx.Product);
+    //    var controller = new ProductsController(mockContext.Object);
+
+    //    // Act
+    //    var result = await controller.Index(null, null);
+
+    //    // Assert
+    //    var viewResult = Assert.IsType<ViewResult>(result);
+    //    var model = Assert.IsAssignableFrom<PaginatedList<Product>>(viewResult.ViewData.Model);
+    //    Assert.Equal(3, model.Count);
+    //}
 
     [Fact]
-    // Checks the returned result
-    public async Task CreateTest_Returns_RedirectToActionResult()
+    public async Task Details_ReturnsNotFoundResult_WhenIdIsNull()
     {
         // Arrange
-        var moker = new Mocker();
-        var sut = moker.Build();
+        var mockContext = new Mock<IShoppingSystemWebContext>();
+        var controller = new ProductsController(mockContext.Object);
 
         // Act
-        var result = await sut.Create(_testProduct);
-        var resultType = Assert.IsType<RedirectToActionResult>(result).ToString();
+        var result = await controller.Details(null);
 
         // Assert
-        Assert.Equal(result.GetType().ToString(), resultType);
+        Assert.IsType<NotFoundResult>(result);
     }
 
     [Fact]
-    // Tests calls to the Add and SaveChangesAsync methods
-    public async Task CreateTest_saves_a_Product_via_context()
+    public async Task Create_ReturnsRedirectToActionResult_WhenModelStateIsValid()
     {
         // Arrange
-        var moker = new Mocker();
-        var sut = moker.Build();
+        var mockContext = new Mock<IShoppingSystemWebContext>();
+        var mockDbSet = new Mock<DbSet<Product>>();
+        mockContext.Setup(c => c.Product).Returns(mockDbSet.Object);
+
+        var controller = new ProductsController(mockContext.Object);
 
         // Act
-        var result = await sut.Create(_testProduct);
+        var result = await controller.Create(new Product
+        {
+            Id = 123,
+            Title = "test title",
+            Category = "test category",
+            ExpiredDate = DateTime.Now,
+            Price = 13.13m
+        });
 
         // Assert
-        moker.MockProductsContext.Verify(m => m.Add(It.IsAny<Product>()), Times.Once());
-        moker.MockProductsContext.Verify(m => m.SaveChangesAsync(It.IsAny<CancellationToken>()), Times.Once());
+        var redirectToActionResult = Assert.IsType<RedirectToActionResult>(result);
+        Assert.Equal("Index", redirectToActionResult.ActionName);
+    }
+    [Fact]
+    public async Task Create_AddsProductToMock_WhenModelStateIsValid()
+    {
+        // Arrange
+        var mockContext = new Mock<IShoppingSystemWebContext>();
+        var mockDbSet = new Mock<DbSet<Product>>();
+        mockContext.Setup(c => c.Product).Returns(mockDbSet.Object);
+
+        var controller = new ProductsController(mockContext.Object);
+        var testProduct = new Product
+        {
+            Id = 123,
+            Title = "test title",
+            Category = "test category",
+            ExpiredDate = DateTime.Now,
+            Price = 13.13m
+        };
+
+        // Act
+        await controller.Create(testProduct);
+
+        // Assert
+        mockDbSet.Verify(dbSet => dbSet.Add(It.Is<Product>(p => p == testProduct)), Times.Once);
+        mockContext.Verify(context => context.SaveChangesAsync(CancellationToken.None), Times.Once);
     }
 
 
     [Fact]
-    // Checks the returned result
-    public async Task DetailsTest_Returns_RedirectToActionResult()
+    public async Task Edit_ReturnsNotFoundResult_WhenIdIsNull()
     {
         // Arrange
-        var moker = new Mocker();
-        var sut = moker.Build();
+        var mockContext = new Mock<IShoppingSystemWebContext>();
+        var controller = new ProductsController(mockContext.Object);
 
         // Act
-
-        var result = await sut.Details(ID); // EXEPTION!!!
-
-        var resultType = Assert.IsType<RedirectToActionResult>(result).ToString();
+        var result = await controller.Edit(null);
 
         // Assert
-        Assert.Equal(result.GetType().ToString(), resultType);
+        Assert.IsType<NotFoundResult>(result);
     }
-
 }
-
-
