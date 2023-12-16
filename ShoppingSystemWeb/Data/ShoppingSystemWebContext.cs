3b5af230ddc491e9bc17dccfa50b0df78ed2a745
HlibBondarev <Hlib.Bondarev@gmail.com>
2023-12-16 19:10:05 +0200

    review

--- ShoppingSystemWeb/Data/ShoppingSystemWebContext.cs
+++ ShoppingSystemWeb/Data/ShoppingSystemWebContext.cs
@@ -10,18 +10,11 @@ namespace ShoppingSystemWeb.Data
 {
     public class ShoppingSystemWebContext : DbContext, IShoppingSystemWebContext
     {
-		//private DbContextOptionsBuilder<ShoppingSystemWebContext> option;
-
 		public ShoppingSystemWebContext (DbContextOptions<ShoppingSystemWebContext> options)
             : base(options)
         {
 			Database.EnsureCreated();
         }
-
-		//public ShoppingSystemWebContext(DbContextOptionsBuilder<ShoppingSystemWebContext> option)
-		//{
-		//	this.option = option;
-		//}
 
 		public virtual DbSet<Product> Product { get; set; }
     }
